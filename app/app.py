dba2369572f581b07c6523a9c9689dd34218a4a2
falk211 <wfalk211@gmail.com>
2025-04-22 21:40:29 -0500

    beginning of login and signup

--- app/app.py
+++ app/app.py
@@ -2,9 +2,38 @@ from flask import Flask, render_template, jsonify, request
 import os
 import psycopg
 
+from flask import redirect, url_for, flash
+
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app.secret_key = "your-very-secret-key"
 @app.route('/')
-def home():
+def loginpage():
+    return render_template('login.html')
+
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    print(username, password)
+
+
+    if username == "admin" and password == "pass123":
+        return redirect(url_for("homepage"))
+    else:
+        return render_template("login.html", error="Invalid username or password")
+
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signuppage():
+    return render_template('signup.html')
+
+@app.route('/homepage')
+def homepage():
     return render_template('index.html')
 
 
